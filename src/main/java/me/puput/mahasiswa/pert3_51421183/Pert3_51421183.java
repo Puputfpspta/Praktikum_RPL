24892a1558782b942e49a17afc418c2e29f7b66a
puputfpspta <puputfpspta@gmail.com>
2024-12-02 18:56:52 +0700

    pert3_51421183

---
+++ src/main/java/me/puput/mahasiswa/pert3_51421183/Pert3_51421183.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package me.puput.mahasiswa.pert3_51421183;
+
+/**
+ *
+ * @author user
+ */
+public class Pert3_51421183 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
